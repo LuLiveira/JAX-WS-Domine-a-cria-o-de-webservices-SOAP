376fd5cca96f0708bb5a6e8b5c1e09c5a3e07cc3
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-12-29 18:33:49 -0300

    O primeiro serviço SOAP

---
+++ src/br/com/caelum/estoque/ws/PublicaEstoqueWS.java
@@ -0,0 +1,17 @@
+package br.com.caelum.estoque.ws;
+
+import javax.xml.ws.Endpoint;
+
+public class PublicaEstoqueWS {
+
+	public static void main(String[] args) {
+
+		EstoqueWS implementacaoWS = new EstoqueWS();
+		String url = "http://localhost:8080/estoquews";
+		
+		System.out.println("EstoqueWS running on: " + url);
+		
+		Endpoint.publish(url, implementacaoWS);
+	}
+
+}
